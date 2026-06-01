db8a9d740e0556b2452b2ae96fd1647ef6bd3d3a
chohh7391 <chohh7391@gmail.com>
2026-06-01 17:02:21 +0900

    modified:   cho_robots_bringup/cho_franka_bringup/launch/bringup_gazebo_robot.launch.py 	modified:   cho_robots_bringup/cho_franka_bringup/launch/bringup_real_robot.launch.py 	modified:   cho_robots_bringup/cho_franka_bringup/launch/bringup_real_robot_backup.launch.py 	modified:   cho_robots_description/cho_franka_description/robots/common/franka_robot.xacro 	modified:   cho_robots_description/cho_franka_description/urdf/fr3_with_ft_sensor/fr3_franka_hand.urdf

--- cho_robots_bringup/cho_franka_bringup/launch/bringup_gazebo_robot.launch.py
+++ cho_robots_bringup/cho_franka_bringup/launch/bringup_gazebo_robot.launch.py
@@ -125,7 +125,8 @@ def generate_launch_description():
                 'ee_id': franka_hand_str,
                 'gazebo_effort': gazebo_effort_str,
                 'special_connection': 'ft_sensor',
-                'xyz_ee': '0 0 0.0175',
+                'xyz_ee': '0 0 -0.0175',
+                'rpy_ee': '3.141592653589793 0 0.7853981633974483',
             }
         )
         
--- cho_robots_bringup/cho_franka_bringup/launch/bringup_real_robot_backup.launch.py
+++ cho_robots_bringup/cho_franka_bringup/launch/bringup_real_robot_backup.launch.py
@@ -135,7 +135,8 @@ def generate_robot_nodes(context):
                 'use_fake_hardware':    use_fake_hw_str,
                 'fake_sensor_commands': fake_sensor_cmds_str,
                 'special_connection':   'ft_sensor',
-                'xyz_ee':               '0 0 0.0175',
+                'xyz_ee':               '0 0 -0.0175',
+                'rpy_ee':               '3.141592653589793 0 0.7853981633974483',
             },
         ).toprettyxml(indent='  ')
 
